13803f482d3fd9ac0b8110c0cf8064d3332e3983
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-15 15:21:50 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,17 +1,19 @@
 def find_max_value(array)
-  # i = 0
-  # j = 0
-  # while i < array.length do 
-  #   while j < array.length do
-  #   if array[i] < array[j]
-  #     bigger_number = array[j]
-  #   end
-  #   i += 1
-  #   j += 1
-  #   return bigger_number
-  #   puts i, j
-  # end
-  # end
-  sorted_arr = array.sort
-  return sorted_arr.pop
+  i = 0
+  j = 0
+  while i < array.length do 
+    while j < array.length do
+    if array[i] < array[j]
+      bigger_number = array[j]
+    end
+    i += 1
+    j += 1
+    return bigger_number
+    puts i, j
+  end
+  end
+  
+  
+  # sorted_arr = array.sort
+  # return sorted_arr.pop
 end
